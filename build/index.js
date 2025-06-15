32598c85c6b6ea026db7511820a7df3002a769ef
guiihxyz <cguilhermecosta27@gmail.com>
2025-06-15 18:54:07 -0300

    fix CORS

--- build/index.js
+++ build/index.js
@@ -2,15 +2,21 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 import autoload from "@fastify/autoload";
 import path from "node:path";
+import { fileURLToPath } from "url";
 import log from "consola";
 import ck from "chalk";
 import "#settings/env.js";
 import { errorHandler } from "#functions/error.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = fastify();
 app.setErrorHandler(errorHandler);
-app.register(cors, { origin: "*" });
-app.register(autoload, {
-    dir: path.join(import.meta.dirname, "routes"),
+await app.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+});
+await app.register(autoload, {
+    dir: path.join(__dirname, "routes"),
     routeParams: true
 });
 app.addHook("onRoute", ({ method, path }) => {
@@ -19,7 +25,7 @@ app.addHook("onRoute", ({ method, path }) => {
     }
     log.success(`${ck.yellow(method)} ${ck.blue(path)}`);
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 await app.listen({ port, host: "0.0.0.0" })
     .catch(err => {
     log.error(err);
